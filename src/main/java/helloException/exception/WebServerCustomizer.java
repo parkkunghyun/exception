3a2a1b1e929ef6fb8e5d2165e14f6aa457e5329e
baggyeonghyeon <rudgus4620@gmail.com>
2023-04-23 16:10:57 +0900

    Initial commit

---
+++ src/main/java/helloException/exception/WebServerCustomizer.java
@@ -0,0 +1,22 @@
+package helloException.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        // WAS에서 다시 이 컨트롤러 경로까지 돌아감!!
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        // response.sendError(500),  RuntimeException의 경우 자식도 처리해줌!
+        factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
+    }
+}
---
+++ src/test/java/helloException/exception/ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package helloException.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
